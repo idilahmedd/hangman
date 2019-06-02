c46502adeeada7bae9176de475b228244028e559
Idilahmed <idil@mac-books-MacBook-Pro.local>
2019-06-02 13:49:26 -0700

    stuck on js

---
+++ js/script.js
@@ -0,0 +1,66 @@
+// variables for the word, guessed letters, default turns, 
+//    errors remaining
+
+// what the app does: 
+// 1. user submits a letter
+// 2. check if letter is in a word (conditional if no letter, return(increment) error and 
+// draw hangman and check if game over
+// ...if letter is in word, )
+// 3. display the current state of the game
+
+
+//function should be 10 of less lines
+
+//set up for JS
+
+//variab declarations on top
+
+ 
+//then HTML element refreneces
+
+//then create all event listeners
+
+//any additional functions to make it work (logic)
+
+
+
+// ** when writing functions dont write function and 
+//function but easier to read
+
+//** all var on top */
+
+// Create an array of random words for source of secret word
+var words = [
+    "cookies",
+    "passion",
+    "caramel",
+    "coffee"
+    ];
+var word = words[Math.floor(Math.random() * words.length)]
+var trys = "myLives"; 
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+var remainingLetters = word.length;
+
+
+
+
+var inputString = "";
+var inputEl = document.querySelector("input");
+var pEl = document.getElementById("myLives");
+
+
+btnEl1.addEventListener("click", function(e) {
+  var letterInput = inputElLtr.value;
+  inputElLtr.value = "input"; 
+  guessingLetter = guessingLetter + letterInput;
+  pEl1.answerArray = guessingLetter;
+});
+
+
+
+
+
